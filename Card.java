dbdc7a2d301e4a3c1d6ce52d404074870913fa77
BrendanStory <storybt@s.dcsdk12.org>
2013-10-29 09:07:21 -0600

    Card and Deck created

---
+++ Card.java
@@ -0,0 +1,24 @@
+class Card {
+	private int cardValue;
+	private Boolean faceDown;
+	private String suit;
+
+	public card(int cardValue, String suit){
+		setValue(cardValue);
+		setSuit(suit);
+	}
+
+	public int cardValue(){
+		return cardValue;
+	}
+	public void setValue(int cardValue){
+		this.cardValue = cardValue;
+	}
+
+	public String suit(){
+		return suit;
+	}
+	public void setSuit(String suit){
+		this.suit = suit;
+	}
+}
